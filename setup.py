20099fac6d2457ccd3ae1252338053c7f20c025f
irtazaakram <irtaza.akram@arbisoft.com>
2023-06-27 13:57:36 +0500

    feat: Add support for django 4.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
     install_requires=[
-        'django>=2.0'
+        'django>=3.2'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -30,10 +30,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP',
     ],
 )
